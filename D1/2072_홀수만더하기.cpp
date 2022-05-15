63bdc8587ee2a86960a85536e1710da0b2110d6c
suna <ssookkookk@naver.com>
2022-05-15 12:15:38 +0900

    modified 2072_홀수만더하기.cpp

--- D1/2072_홀수만더하기.cpp
+++ D1/2072_홀수만더하기.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
-int main(void)
+int main(int argc, char** argv)
 {
-	int idx = 1;
-	int T = 0;
+	int test_case;
+	int T;
+	cin >> T;
 	int sum = 0;
 	int arr[10];
-	scanf("%d", &T);
-	while (idx <= T)
+	for(test_case = 1; test_case <= T; ++test_case)
 	{
 		for (int i = 0; i < 10; i++)
 		{
@@ -16,8 +17,8 @@ int main(void)
 			if (arr[i] % 2 == 1)
 				sum += arr[i];
 		}
-		cout << "#" << idx << " " << sum << "\n";
+		cout << "#" << test_case << " " << sum << "\n";
 		sum = 0;
-		idx++;
 	}
+	return 0;
 }
